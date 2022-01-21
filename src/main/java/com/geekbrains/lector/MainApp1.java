86ccdf0f20f4b298c3d31901542ed6b1529f3fe9
Sergey <forgit@list.ru>
2022-01-21 20:24:31 +0300

    21/01/22

--- src/main/java/com/geekbrains/lector/MainApp1.java
+++ src/main/java/com/geekbrains/lector/MainApp1.java
@@ -2,6 +2,7 @@ package com.geekbrains.lector;
 
 import java.io.File;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.net.URL;
@@ -9,8 +10,14 @@ import java.net.URLClassLoader;
 import java.util.Arrays;
 
 public class MainApp1 {
-    public static void main(String[] args) {
-        Class testClass = new TestClass.class;
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        Class testClass = TestClass.class;
         Method[] methods = testClass.getDeclaredMethods();
+        for (Method o:methods){
+            if(o.isAnnotationPresent(MyAnnotation.class)){
+                o.invoke(null);//это статический метод, поэтому делаем null
+                //при запуске выведет: method1 method2
+            };
+        }
     }
 }
