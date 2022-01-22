981d2ed59bc2324eee51efe28007dfa813f6c6d0
Forgit1212 <forgit@list.ru>
2022-01-22 11:19:47 +0300

    22.01.22

--- src/main/java/com/geekbrains/lector/MainApp1.java
+++ src/main/java/com/geekbrains/lector/MainApp1.java
@@ -7,16 +7,20 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MainApp1 {
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
         Class testClass = TestClass.class;//этот код по сути - обработчик аннотаций
         Method[] methods = testClass.getDeclaredMethods();
+        List<Method> executionList = new ArrayList<>();
         for (Method o:methods){
             if(o.isAnnotationPresent(MyAnnotation.class)){
                 o.invoke(null);//это статический метод, поэтому делаем null
                 //при запуске выведет: method1 method2
+                executionList.add(o);
             };
         }
     }
--- src/main/java/com/geekbrains/lector/TestClass.java
+++ src/main/java/com/geekbrains/lector/TestClass.java
@@ -2,17 +2,18 @@ package com.geekbrains.lector;
 
 public class TestClass {
 
-    @MyAnnotation(priority = 10)
+    @MyAnnotation(priority = 3)
     public  static void method1 () {
         System.out.println("method 1");
     }
 
     //@Test()//рядом с аннотациями иногда можно размещать данные. Откуда они берутся?
+    @MyAnnotation(priority = 4)
     public  static void method2 () {
         System.out.println("method 2");
     }
 
-    @MyAnnotation(priority = 5)
+    @MyAnnotation(priority = 2)
     public  static void method3 () {
         System.out.println("method 3");
     }
